1fc6e965ebb8cd814e850467e9e452cac76754ec
Daichi ONODERA <onodes@onod.es>
2012-09-25 16:06:38 +0900

    write spec for graph

--- lib/cecelia/graph.rb
+++ lib/cecelia/graph.rb
@@ -2,7 +2,7 @@ require 'sequel'
 require 'yaml'
 
 class Graph
-  def initialize(db_name)
+  def initialize(db_name = "sqlite:/")
     @db = Sequel.connect(db_name)
     require 'cecelia/graph_model.rb'
   end
@@ -15,9 +15,14 @@ class Graph
 
   def add_edge(source, target, attributes = {})
     if source.class == String && target.class == String
-      source_id = Vertices.find(:label => source)[:id]
-      target_id = Vertices.find(:label => target)[:id]
-      add_edge_id(source_id, target_id, attributes = {})
+      begin
+        source_id = Vertices.find(:label => source)[:id]
+        target_id = Vertices.find(:label => target)[:id]
+      rescue => exc
+         exc
+      ensure
+        add_edge_id(source_id, target_id, attributes = {})
+      end
     else
       add_edge_id(source, target, attributes = {})
     end
@@ -36,7 +41,7 @@ class Graph
     if id.class == Integer
       dataset = Edges.filter(:source => id).all
     elsif id.class == String
-      label = Vertices.find(:label = id)[:id]
+      label = Vertices.find(:label => id)[:id]
       dataset = Edges.filter(:source => label).all
     end
     dataset
@@ -45,7 +50,7 @@ class Graph
   def find_vertex(args = {})
     Vertices.find(args)
   end
-  
+
   def find_edge(args = {})
     Edges.find(args)
   end
@@ -56,6 +61,17 @@ class Graph
 
   def filter_edge(args = {})
     Edges.filter(args)
+  end
+
+  def remove_vertex(label)
+    if label.class == String 
+      Vertices.find(:label => label).delete
+    end
+  end
+
+  def remove_edge(id)
+    id = id.to_i  
+    Edges.find(:id => id).delete
   end
 
   private
---
+++ spec/graph_spec.rb
@@ -0,0 +1,126 @@
+#encoding: utf-8
+require 'rspec'
+require './lib/cecelia/graph.rb'
+
+
+describe Graph,"Iniaialize method" do
+  it "Graph.new()に空引数" do
+    Graph.new().should be_true
+  end
+
+  it "Graph.new()にSQLiteを指定" do
+    Graph.new("sqlite://hoge.db").should be_true
+  end
+
+  after(:all) do
+    #File.delete("./hoge.db")
+  end
+end
+
+describe Graph, "ノード追加に関するテスト" do
+  let(:g){Graph.new}
+
+  it "空グラフのノード数" do 
+    g.vertices.size.should == 0
+  end
+
+  it "ノード一つ追加" do
+    g.add_vertex("0").should be_true
+  end
+
+  it "グラフのノード数確認" do
+    g.vertices.size.should == 1
+  end
+
+  it "グラフに複数ノード追加" do
+    10.times{|i| g.add_vertex(i.to_s)}.should be_true
+  end
+
+  it "グラフの複数ノード数確認" do 
+    g.vertices.size.should == 10 
+  end
+
+  it "同名ノードの追加" do
+    10.times{|i|
+      g.add_vertex(i.to_s)
+    }.should be_true
+  end
+
+  it "同名ノードの禁止" do
+    g.vertices.size.should == 10
+  end
+end
+
+describe Graph,"エッジの追加に関するテスト" do
+  let(:g){Graph.new}
+
+  #before(:all){10.times{|i| g.add_vertex(i.to_s)}}
+
+  it "空グラフのエッジ数" do
+    g.edges.size.should == 0
+  end
+
+  it "エッジを1本作成" do
+    g.add_vertex("1")
+    g.add_vertex("2")
+    g.add_edge("1","2")
+    g.edges.size.should == 1
+  end
+
+  it "存在しないノード間エッジは禁止" do
+    g.add_edge("100","200").should raise_error
+  end
+
+  it "複数ノードの追加" do
+    10.times{|i|
+      g.add_vertex(i.to_s)
+    }.should be_true
+  end
+
+  it "複数エッジの追加" do
+    g.add_edge("3","4")
+    g.add_edge("5","6")
+    g.add_edge("6","7")
+    g.add_edge("1","6")
+
+    g.edges.size.should == 5
+  end
+end
+
+describe Graph,"ノード削除に関するテスト" do
+  let(:g){Graph.new}
+
+  before(:all){
+    10.times do |i|
+    g.add_vertex(i.to_s)
+    end
+  }
+
+
+  it "ノードを1つ削除" do
+    size = g.vertices.size
+    g.remove_vertex("1")
+    (size - g.vertices.size).should == 1
+  end
+end
+
+describe Graph,"エッジ削除に関するテスト" do
+
+  let(:g){Graph.new}
+
+  before(:all){
+    10.times do |i|
+    g.add_vertex(i.to_s)
+    end
+
+  g.add_edge("1","2")
+  g.add_edge("3","4")
+  g.add_edge("5","6")
+  }
+
+  it "エッジを1つ削除" do
+    size = g.edges.size
+    g.remove_edge("1")
+    (size - g.edges.size).should == 1
+  end
+end
